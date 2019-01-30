7a0292349d473f7c30158c3fcf6d12d8c106d281
kjkrVdxB <kjkrVdxB@airmail.cc>
2019-01-31 01:14:35 +0300

    Add interface documentation

--- trie/src/main/java/com/example/trie/Serializable.java
+++ trie/src/main/java/com/example/trie/Serializable.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/** Interface for custom serialization. */
 public interface Serializable {
+    /** Serialize the object to {@code out}. */
     void serialize(OutputStream out) throws IOException;
+
+    /** Replace current object with the object deserialized from {@code in}. */
     void deserialize(InputStream in) throws IOException;
 }
