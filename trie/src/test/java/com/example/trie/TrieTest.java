614813147e682e6e36e23212fba445aee7d77be3
kjkrVdxB <kjkrVdxB@airmail.cc>
2019-02-15 23:57:06 +0300

    Optimize imports

--- trie/src/test/java/com/example/trie/TrieTest.java
+++ trie/src/test/java/com/example/trie/TrieTest.java
@@ -7,7 +7,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
