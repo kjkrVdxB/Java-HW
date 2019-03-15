b5230c3d2d0cc1b8c48a4314de00803472edc633
kjkrVdxB <kjkrVdxB@airmail.cc>
2019-03-15 23:43:21 +0300

    Document NavigableSet

--- treeset/src/main/java/com/example/treeset/NavigableSet.java
+++ treeset/src/main/java/com/example/treeset/NavigableSet.java
@@ -3,14 +3,13 @@ package com.example.treeset;
 import java.util.Iterator;
 import java.util.Set;
 
+/** An interface with some of the methods of {@link java.util.NavigableSet}. */
 public interface NavigableSet<E> extends Set<E> {
-
     /** {@link java.util.NavigableSet#descendingIterator} */
     Iterator<E> descendingIterator();
 
     /** {@link java.util.NavigableSet#descendingSet} */
     NavigableSet<E> descendingSet();
-
 
     /** {@link java.util.NavigableSet#first} */
     E first();
@@ -18,13 +17,11 @@ public interface NavigableSet<E> extends Set<E> {
     /** {@link java.util.NavigableSet#last} */
     E last();
 
-
     /** {@link java.util.NavigableSet#lower} */
     E lower(E e);
 
     /** {@link java.util.NavigableSet#floor} */
     E floor(E e);
-
 
     /** {@link java.util.NavigableSet#ceiling} */
     E ceiling(E e);
