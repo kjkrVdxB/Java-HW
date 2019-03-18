412601a5fe7f9902e8879f807125d1e4e874f9d1
Travis CI <travis@travis-ci.com>
2019-03-18 17:59:07 +0000

    Auto deploy from Travis CI

---
+++ qsort/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.qsort","l":"Main"},{"p":"com.example.qsort","l":"ParallelQuickSort"},{"p":"com.example.qsort","l":"SingleThreadQuickSort"}]
