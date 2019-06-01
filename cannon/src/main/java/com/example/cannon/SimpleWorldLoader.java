17e7668c0066d5125fe95a1f8a57a7aa86bd8e18
kjkrVdxB <kjkrVdxB@airmail.cc>
2019-06-01 17:06:08 +0300

    Clarify SimpleWorldLoader format

--- cannon/src/main/java/com/example/cannon/SimpleWorldLoader.java
+++ cannon/src/main/java/com/example/cannon/SimpleWorldLoader.java
@@ -17,17 +17,29 @@ public class SimpleWorldLoader implements World.WorldProvider {
     private Cannon cannon;
     private Target target;
 
+    /**
+     * Loads a file with a Cannon game instance in the following text format:
+     *
+     * 'int: number of vertices in terrain'
+     * 'double: vertex 1 X coordinate' 'double: vertex 1 Y coordinate'
+     * 'double: vertex 2 X coordinate' ...
+     * ...
+     * 'double: cannon starting angle' 'double: cannon X coordinate' 'double: cannon Y coordinate'
+     * 'double: target X coordinate' 'double: target Y coordinate'
+     *
+     * @throws FileNotFoundException if the file could not be found
+     */
     public SimpleWorldLoader(@NonNull String filename) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File(filename));
-        var vertices = new ArrayList<Point2D>();
-        int verticesNumber = scanner.nextInt();
-        for (int i = 0; i < verticesNumber; ++i) {
-            vertices.add(new Point2D(scanner.nextDouble(), scanner.nextDouble()));
+        try (var scanner = new Scanner(new File(filename))){
+            var vertices = new ArrayList<Point2D>();
+            int verticesNumber = scanner.nextInt();
+            for (int i = 0; i < verticesNumber; ++i) {
+                vertices.add(new Point2D(scanner.nextDouble(), scanner.nextDouble()));
+            }
+            terrain = new Terrain(vertices);
+            cannon = new Cannon(scanner.nextDouble(), new Point2D(scanner.nextDouble(), scanner.nextDouble()));
+            target = new Target(new Point2D(scanner.nextDouble(), scanner.nextDouble()));
         }
-        terrain = new Terrain(vertices);
-        cannon = new Cannon(scanner.nextDouble(), new Point2D(scanner.nextDouble(), scanner.nextDouble()));
-        target = new Target(new Point2D(scanner.nextDouble(), scanner.nextDouble()));
-        scanner.close();
     }
 
     public Terrain getTerrain() {
