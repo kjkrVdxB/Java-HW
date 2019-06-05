863a3e170839fc581d7da50690af166532905275
Your Name <>
2019-06-05 04:29:32 +0300

    Add direct tests for client

--- p2.cw4/src/test/java/com/example/p2cw4/FTPTest.java
+++ p2.cw4/src/test/java/com/example/p2cw4/FTPTest.java
@@ -4,21 +4,24 @@ import com.example.p2cw4.FTPClient.ListingItem;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
+import java.util.function.Consumer;
 
+import static com.example.p2cw4.FTPServer.REQUEST_GET;
+import static com.example.p2cw4.FTPServer.REQUEST_LIST;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FTPTest {
     private static final int TEST_RUNS = 10;
     private static final int TEST_PORT = 10000;
+    private static final int SECOND_TEST_PORT = 10001;
     private FTPServer server = new FTPServer(TEST_PORT);
     private FTPClient client = new FTPClient();
     @SuppressWarnings("WeakerAccess")
@@ -232,5 +235,174 @@ class FTPTest {
 
         otherClient.disconnect();
         yetAnotherClient.disconnect();
+    }
+
+    @Test
+    void testClientGetRequestDirect() throws IOException, InterruptedException {
+        var otherClient = new FTPClient();
+
+        var expectedRequest = constructPacket(data -> {
+            try {
+                data.writeInt(REQUEST_GET);
+                data.writeUTF("abacaba");
+            } catch (IOException ignore) {
+            }
+        });
+
+        var answer = constructPacket(data -> {
+            try {
+                data.writeInt(-1);
+                data.write(new byte[]{1, 2, 3, 4});
+            } catch (IOException ignore) {
+            }
+        });
+
+        var simpleServer = new SimpleServer(expectedRequest.length, answer, SECOND_TEST_PORT);
+        otherClient.connect("localhost", SECOND_TEST_PORT);
+        assertThrows(FileNotFoundException.class, () -> otherClient.executeGet("abacaba"));
+        byte[] clientRequest = simpleServer.getResult();
+
+        assertArrayEquals(expectedRequest, clientRequest);
+
+        otherClient.disconnect();
+    }
+
+    @Test
+    void testClientGetAnswerDirect() throws IOException, InterruptedException {
+        var otherClient = new FTPClient();
+
+        var expectedRequest = constructPacket(data -> {
+            try {
+                data.writeInt(REQUEST_GET);
+                data.writeUTF("abacaba");
+            } catch (IOException ignore) {
+            }
+        });
+
+        var answer = constructPacket(data -> {
+            try {
+                data.writeInt(4);
+                data.write(new byte[]{1, 2, 3, 4});
+            } catch (IOException ignore) {
+            }
+        });
+
+        var simpleServer = new SimpleServer(expectedRequest.length, answer, SECOND_TEST_PORT);
+        otherClient.connect("localhost", SECOND_TEST_PORT);
+        assertArrayEquals(new byte[]{1, 2, 3, 4}, otherClient.executeGet("abacaba"));
+        byte[] clientRequest = simpleServer.getResult();
+
+        assertArrayEquals(expectedRequest, clientRequest);
+
+        otherClient.disconnect();
+    }
+
+    @Test
+    void testClientListRequestDirect() throws IOException, InterruptedException {
+        var otherClient = new FTPClient();
+
+        var expectedRequest = constructPacket(data -> {
+            try {
+                data.writeInt(REQUEST_LIST);
+                data.writeUTF("abacaba");
+            } catch (IOException ignore) {
+            }
+        });
+
+        var answer = constructPacket(data -> {
+            try {
+                data.writeInt(-1);
+            } catch (IOException ignore) {
+            }
+        });
+
+        var simpleServer = new SimpleServer(expectedRequest.length, answer, SECOND_TEST_PORT);
+        otherClient.connect("localhost", SECOND_TEST_PORT);
+        assertThrows(FileNotFoundException.class, () -> otherClient.executeList("abacaba"));
+        byte[] clientRequest = simpleServer.getResult();
+
+        assertArrayEquals(expectedRequest, clientRequest);
+
+        otherClient.disconnect();
+    }
+
+    @Test
+    void testClientListAnswerDirect() throws IOException, InterruptedException {
+        var otherClient = new FTPClient();
+
+        var expectedRequest = constructPacket(data -> {
+            try {
+                data.writeInt(REQUEST_LIST);
+                data.writeUTF("abacaba");
+            } catch (IOException ignore) {
+            }
+        });
+
+        var answer = constructPacket(data -> {
+            try {
+                data.writeInt(2);
+                data.writeUTF("a");
+                data.writeBoolean(true);
+                data.writeUTF("b");
+                data.writeBoolean(false);
+            } catch (IOException ignore) {
+            }
+        });
+
+        var simpleServer = new SimpleServer(expectedRequest.length, answer, SECOND_TEST_PORT);
+        otherClient.connect("localhost", SECOND_TEST_PORT);
+        assertEquals(List.of(new ListingItem(ListingItem.Type.DIRECTORY, "a"),
+                             new ListingItem(ListingItem.Type.FILE, "b")), otherClient.executeList("abacaba"));
+        byte[] clientRequest = simpleServer.getResult();
+
+        assertArrayEquals(expectedRequest, clientRequest);
+
+        otherClient.disconnect();
+    }
+
+    private byte[] constructPacket(Consumer<DataOutputStream> streamWriter) {
+        var packetBody = new ByteArrayOutputStream();
+        var packetBodyData = new DataOutputStream(packetBody);
+
+        streamWriter.accept(packetBodyData);
+
+        var packet = new ByteArrayOutputStream();
+        var packetData = new DataOutputStream(packet);
+
+        try {
+            packetData.writeInt(packetBody.size());
+            packetData.write(packetBody.toByteArray());
+        } catch (IOException ignored) {
+        }
+
+        return packet.toByteArray();
+    }
+
+    private static class SimpleServer {
+        private ServerSocket serverSocket;
+        private Thread serverThread;
+        private ByteArrayOutputStream result = new ByteArrayOutputStream();
+        private SimpleServer(int numberBytesToReceive, byte[] bytesToWrite, int port) throws IOException {
+            serverSocket = new ServerSocket(port);
+            serverThread = new Thread(() -> {
+                try {
+                    var socket = serverSocket.accept();
+                    var socketInput = socket.getInputStream();
+                    for (int i = 0; i < numberBytesToReceive; ++i) {
+                        var byteRead = socketInput.read();
+                        result.write(byteRead);
+                    }
+                    var socketOutput = socket.getOutputStream();
+                    socketOutput.write(bytesToWrite);
+                } catch (IOException ignore) {
+                }
+            });
+            serverThread.start();
+        }
+
+        private byte[] getResult() throws InterruptedException {
+            serverThread.join();
+            return result.toByteArray();
+        }
     }
 }
