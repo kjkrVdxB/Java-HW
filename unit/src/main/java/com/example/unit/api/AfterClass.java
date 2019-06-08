8d330dd1e89d0577ee36ae2f27b641ee4d11f72e
kjkrVdxB <kjkrVdxB@airmail.cc>
2019-06-08 21:08:45 +0300

    Add documentation to api annotations

--- unit/src/main/java/com/example/unit/api/AfterClass.java
+++ unit/src/main/java/com/example/unit/api/AfterClass.java
@@ -2,6 +2,10 @@ package com.example.unit.api;
 
 import java.lang.annotation.*;
 
+/**
+ * Methods in a test class annotated with this annotation will be run before all tests once.
+ * Such methods must be static.
+ */
 @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AfterClass {
--- unit/src/main/java/com/example/unit/api/Test.java
+++ unit/src/main/java/com/example/unit/api/Test.java
@@ -5,9 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Methods in a test class annotated with this annotation will be run as tests.
+ * Such methods must not be static.
+ */
 @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test {
+    /** Whether the test should be ignored. The value is the reason of it being ignored. */
     String ignore() default "";
+
+    /**
+     * Set to the exception expected to be thrown during the test invocation. {@code NoExceptionExpected.class} if no
+     * exception is expected.
+     */
     Class<? extends Exception> expected() default NoExceptionExpected.class;
 }
