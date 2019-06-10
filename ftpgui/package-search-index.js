7b46a627e69ab52e4924a0183710184b57c690b1
Travis CI <travis@travis-ci.com>
2019-06-10 12:03:00 +0000

    Auto deploy from Travis CI

--- ftpgui/package-search-index.js
+++ ftpgui/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.ftpgui"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.ftpgui"},{"l":"com.example.ftpgui.server"}]
